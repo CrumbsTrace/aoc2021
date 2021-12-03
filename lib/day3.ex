756905feed67e5e26bd0e1e29464ae5dbf6bbbb0
CrumbsTrace <45168144+CrumbsTrace@users.noreply.github.com>
2021-12-03 19:29:14 +0100

    cleanup

--- lib/day3.ex
+++ lib/day3.ex
@@ -8,12 +8,21 @@ defmodule Aoc2021.Day3 do
     iex> Aoc2021.Day3.p2("inputs/day3.txt")
     4636702
   """
+  # Part 1
   def p1(file) do
     parse_input(:lines_characters, file)
     |> Enum.map(&parse_bit_strings/1)
+    |> get_common_bits()
     |> get_gamma_and_epsilon()
   end
 
+  defp get_gamma_and_epsilon(common_bits) do
+    gamma = unbits(common_bits)
+    epsilon = Enum.map(common_bits, &(1 - &1)) |> unbits()
+    gamma * epsilon
+  end
+
+  # Part 2
   def p2(file) do
     parse_input(:lines_characters, file)
     |> Enum.map(&parse_bit_strings/1)
@@ -32,26 +41,14 @@ defmodule Aoc2021.Day3 do
     if length(list) == 1 do
       list
     else
-      bit = get_bit_row(list, index) |> most_significant_bit()
-      bit = if invert, do: 1 - bit, else: bit
-      Enum.filter(list, &(Enum.at(&1, index) == bit))
+      common_bit = get_bit_row(list, index) |> common_bit()
+      check_value = if invert, do: 1 - common_bit, else: common_bit
+      Enum.filter(list, &(Enum.at(&1, index) == check_value))
     end
   end
 
-  defp get_bit_row(list, index), do: list |> Enum.zip() |> Enum.at(index)
-
-  defp get_gamma_and_epsilon(bitstrings) do
-    significant_bits =
-      bitstrings
-      |> Enum.zip()
-      |> Enum.map(&most_significant_bit/1)
-
-    gamma = unbits(significant_bits)
-    epsilon = Enum.map(significant_bits, &(1 - &1)) |> unbits()
-    gamma * epsilon
-  end
-
-  defp most_significant_bit(bits) do
+  # Generic helpers
+  defp common_bit(bits) do
     bit_list = Tuple.to_list(bits)
     total_count = Enum.count(bit_list)
     ones = Enum.count(bit_list, &(&1 == 1))
@@ -64,4 +61,8 @@ defmodule Aoc2021.Day3 do
   end
 
   defp parse_bit_strings(bit_strings), do: Enum.map(bit_strings, &String.to_integer/1)
+
+  defp get_bit_row(list, index), do: list |> Enum.zip() |> Enum.at(index)
+
+  defp get_common_bits(bitstrings), do: bitstrings |> Enum.zip() |> Enum.map(&common_bit/1)
 end
