20caf36a12fa8f29a369a14104796d3003b79e4f
CrumbsTrace <45168144+CrumbsTrace@users.noreply.github.com>
2021-12-17 20:58:54 +0100

    Slight optimizations. Removed tuples since they allocate significantly more memory each cycle

--- lib/day17.ex
+++ lib/day17.ex
@@ -17,23 +17,25 @@ defmodule Aoc2021.Day17 do
   def p2(file) do
     [x_min, x_max, y_min, y_max] = parse(file)
 
-    for x <- 0..x_max, y <- y_min..abs(y_min + 1), reduce: 0 do
-      acc -> if hits?({0, 0}, {x, y}, [x_min, x_max, y_min, y_max]), do: acc + 1, else: acc
+    for x <- lower_bound_x(x_min)..x_max, y <- y_min..abs(y_min + 1), reduce: 0 do
+      acc -> if hits?(0, 0, x, y, [x_min, x_max, y_min, y_max]), do: acc + 1, else: acc
     end
   end
 
-  defp hits?({p_x, p_y} = p, v, [x_min, x_max, y_min, y_max] = range) do
+  defp hits?(p_x, p_y, v_x, v_y, [x_min, x_max, y_min, y_max] = range) do
     cond do
       p_x > x_max or p_y < y_min -> false
-      p_x < x_min or p_y > y_max -> hits?(move(p, v), update_velocity(v), range)
+      p_x < x_min or p_y > y_max -> hits?(p_x + v_x, p_y + v_y, new_v_x(v_x), v_y - 1, range)
       true -> true
     end
   end
 
-  def move({p_x, p_y}, {v_x, v_y}), do: {p_x + v_x, p_y + v_y}
+  # The minimum x to reach the field is would be where the sum of integers for this value reaches the left bound
+  # This is an approximation that guarantees to be less than this value but pretty close
+  defp lower_bound_x(x_min), do: floor(:math.sqrt(x_min * 2))
 
-  def update_velocity({0, v_y}), do: {0, v_y - 1}
-  def update_velocity({v_x, v_y}), do: {v_x - 1, v_y - 1}
+  def new_v_x(0), do: 0
+  def new_v_x(v_x), do: v_x - 1
 
   defp parse(file) do
     hd(parse_input(:lines, file))
